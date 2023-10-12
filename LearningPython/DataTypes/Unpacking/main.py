8126c2f831d1048e73b3b8e575583cd2243869d0
ARGHouse <34590049+ARGHouse@users.noreply.github.com>
2023-10-12 16:08:34 +1100

    Add files via upload

---
+++ LearningPython/DataTypes/Unpacking/main.py
@@ -0,0 +1,13 @@
+a, b, c = (10,5,1)
+print(a)
+print(b)
+print(c)
+print(a + b + c)
+
+sys, eng, wep = 6, 12, 6
+print('weapons is at', wep, 'pips')
+
+val_1 = 9000
+val_2 = 'some text'
+val_1, val_2 = val_2, val_1
+print(val_1)
